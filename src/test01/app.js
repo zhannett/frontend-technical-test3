23dbb6352d1eacb825942199b2db0f4bee23e3cd
zhannett <janet@hubub.com>
2017-04-22 22:34:38 -0400

    Added assignments 2,3 for test01

--- src/test01/app.js
+++ src/test01/app.js
@@ -1,8 +1,15 @@
 export const test01 = {
   template: require('./app.html'),
-  controller() {
-    
-    // your code here
-
+  controller($http) {
+    const $ctrl = this;
+    $http.get('http://localhost:3000/test01/data.json')
+      .then(
+        function(response) {
+          $ctrl.data = response;
+        },
+        function (response) {
+          $ctrl.message = "Error: " + response.status + " " + response.statusText;
+        }
+      );
   }
 };
